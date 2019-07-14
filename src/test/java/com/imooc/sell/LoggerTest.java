a8de7231880643d727bc47abe8ba951c884f2103
qinlang <richard_ql@hotmail.com>
2019-07-14 22:51:13 +0800

    add Slf4j logger

---
+++ src/test/java/com/imooc/sell/LoggerTest.java
@@ -0,0 +1,23 @@
+package com.imooc.sell;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class LoggerTest {
+//    private final Logger logger = LoggerFactory.getLogger(LoggerTest.class);
+
+    @Test
+    public void Test1() {
+        String name = "imooc";
+        String password = "123456";
+        log.debug("debug...");
+        log.info("name: {}, password: {}, info...", name, password);
+        log.error("error...");
+    }
+}
